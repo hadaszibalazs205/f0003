3b27600d91ffae0bc10ec56a7b772617d920702e
hadaszibalazs20 <hadaszi.balazs012345@gmail.com>
2020-11-06 07:32:46 +0000

    Initial commit

---
+++ f0003b.py
@@ -0,0 +1,8 @@
+vezetéknév = input('Mi a vezetékneved?')
+keresztnév = input('Mi a keresztneved?')
+print('A te neved', vezetéknév, keresztnév, '.')
+
+születésiév = input('Mikor születtél?')
+print('A te születési éved', születésiév, '.')
+csillagjegy = input('Mi a csillagjegyed?')
+print('A te csillagjegyed', csillagjegy, '.')
